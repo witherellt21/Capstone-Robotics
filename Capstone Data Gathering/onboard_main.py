0025714abccf00386d64f9b68e6c77cda1edd451
witherellt21 <witherellt21@mail.wlu.edu>
2020-09-28 18:44:06 -0400

    Update onboard main

--- Capstone Data Gathering/onboard_main.py
+++ Capstone Data Gathering/onboard_main.py
@@ -8,6 +8,8 @@ from client import Client
 from sonar import Sonar
 import pygame
 import time
+from imu import IMU
+
 
 # ---------------- Initialize Client -----------------
 client = Client()
@@ -18,46 +20,50 @@ client.PORT = 1234
 
 client.connect()
 
-
 # ---------------- Initialize Pygame -----------------
-pygame.init()
-screen = pygame.display.set_mode((600, 600))
+#pygame.init()
+#screen = pygame.display.set_mode((600, 600))
 msg = 'stop'
 counter = 0
 
 
-# ---------------- Initialize Controller -----------------
-joysticks = []
-pygame.joystick.init()
-
-for i in range(pygame.joystick.get_count()):
-    joysticks.append(pygame.joystick.Joystick(i))
-    joysticks[-1].init()
-
-print(joysticks)
-
-
 # ---------------- Initialize Sonar -----------------
+print('start')
 s = Sonar(18, 24)
+imu = IMU()
+msg = []
 
 running = True
 while running:
 
     counter += 1
 
-    screen.fill((0,0,0))
+    #screen.fill((0,0,0))
 
     #if counter == 10:
     dist = round(s.distance(), 3)
+
+    ag_data_ready = imu.driver.read_ag_status().accelerometer_data_available
+    if ag_data_ready:
+        temp, acc, gyro = imu.read_ag()
+
     time.sleep(.1)
-    msg = "sonar = " + str(dist)
-    print(msg)
-    counter = 0
+
+    msg.append("sonar = " + str(dist))
+    msg.append("temp = " + str(temp))
+    msg.append("acc = " + str(acc))
+    msg.append("gyro = " + str(gyro))
+    print(str(msg))
+    #ounter = 0
     client.send(msg)
 
-    pygame.display.update()
+    #pygame.display.update()
 
     #client.receive()
+    #if counter == 100:
+        #running = False
 
+    msg = []
 
-pygame.quit()
+print('done')
+#pygame.quit()
