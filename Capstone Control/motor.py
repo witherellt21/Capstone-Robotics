d7d00f811ba91bed3be171ef8ebc028a63d7b776
witherellt21 <witherellt21@mail.wlu.edu>
2020-10-19 10:03:19 -0400

    Update motor class for DC control

--- Capstone Control/motor.py
+++ Capstone Control/motor.py
@@ -1,29 +1,66 @@
-"""
-Author: Nick St. Pierre
-Filename: motor.py
-Description: a motor class uses simple forward backward and stop methods for each servo motor.
-"""
 
-import pygame, time
-from gpiozero import Servo
+'''
+STBY = Pin 13 (GPIO #21)
 
+Motor A:
+PWMA = Pin 7 (GPIO #4)
+AIN2 = Pin 11 (GPIO #17)
+AIN1 = Pin 12 (GPIO #18)
 
-class Motor(Servo):
+Motor B:
+BIN1 = Pin 15 (GPIO #22)
+BIN2 = Pin 16 (GPIO #23)
+PWMB = Pin 18 (GPIO #24)
 
-    def __init__ (self, servoPin):
-        "Initializes the servo and has basic drive functions"
-        self.servo = Servo(servoPin)
+'''
+
+# Import required modules
+import time
+import RPi.GPIO as GPIO
+
+class Motor():
+
+    def __init__(self):
+
+        # Declare the GPIO settings
+        GPIO.setmode(GPIO.BOARD)
+
+        self.PWMA = 7
+        self.AIN2 = 11
+        self.AIN1 = 12
+        self.STBY = 13
+
+        # set up GPIO pins
+        GPIO.setup(self.PWMA, GPIO.OUT) # Connected to PWMA
+        GPIO.setup(self.AIN2, GPIO.OUT) # Connected to AIN2
+        GPIO.setup(self.AIN1, GPIO.OUT) # Connected to AIN1
+        GPIO.setup(self.STBY, GPIO.OUT) # Connected to STBY
 
     def forward(self):
-        "Function that drives the servos forward"
-        self.servo.max()
-        time.sleep(0.05)
+        # Drive the motor clockwise
+        GPIO.output(self.AIN1, GPIO.HIGH) # Set AIN1
+        GPIO.output(self.AIN2, GPIO.LOW) # Set AIN2
 
-    def backward(self):
-        "Function that drives the servos backward"
-        self.servo.min()
-        time.sleep(0.05)
+        # Set the motor speed
+        GPIO.output(self.PWMA, GPIO.HIGH) # Set PWMA
+
+        # Disable STBY (standby)
+        GPIO.output(13, GPIO.HIGH)
+
+    def backward(self)
+        # Drive the motor counterclockwise
+        GPIO.output(self.AIN1, GPIO.LOW) # Set AIN1
+        GPIO.output(self.AIN2, GPIO.HIGH) # Set AIN2
+
+        # Set the motor speed
+        GPIO.output(self.PWMA, GPIO.HIGH) # Set PWMA
+
+        # Disable STBY (standby)
+        GPIO.output(self.STBY, GPIO.HIGH)
 
     def stop(self):
-        "Stops motor movement"
-        self.servo.detach()
+        # Reset all the GPIO pins by setting them to LOW
+        GPIO.output(self.AIN1, GPIO.LOW) # Set AIN1
+        GPIO.output(self.AIN2, GPIO.LOW) # Set AIN2
+        GPIO.output(self.PWMA, GPIO.LOW) # Set PWMA
+        GPIO.output(self.STBY, GPIO.LOW) # Set STBY
