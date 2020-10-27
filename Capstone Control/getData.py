3b9d87c7827506a21e0318aa7298a52ae232bd6f
witherellt21 <witherellt21@mail.wlu.edu>
2020-10-27 11:54:09 -0400

    Update getdata

--- Capstone Control/getData.py
+++ Capstone Control/getData.py
@@ -5,48 +5,35 @@ Description: Instantiates the server class and includes methods for receiving da
 and also has the ability for sending data back to the client.
 '''
 
-from server import Server
+from client import Client
 
 class Receiver():
 
     def __init__(self, IP, PORT):
-        self.server = Server(IP, PORT)
-        self.server.start()
-        self.server.receiveConnection()
+        self.client = Client(IP, PORT)
 
-    def receive(self):
-        msg = self.server.receive()
-        self.datalist = []
+    def receive(self, msg):
+        #msg = self.server.receive()
         #msg = 'accel = 20, sonar = 30, temp = 70'
         if not msg == None:
+            #print(msg)
             self.separateData(msg)
+        #if self.server.disconnect_counter == 5:
+            #self.server.receiveConnection()
 
     def send(self, string):
-        self.server.send(string)
+        self.client.send(string)
 
     def separateData(self, msg):
-        self.datalist = msg.split(',,')
+        self.datalist = msg.split(',')
 
-    def getTemp(self, last):
+    def getIMU(self, last):
         for data in self.datalist:
-            if 'temp' in data:
+            if 'IMU' in data:
                 try: return float(data.split('=')[1].strip())
                 except: return last
         return last
 
-    def getAccel(self, last):
-        for data in self.datalist:
-            if 'accel' in data.strip(' '):
-                try: return data.split('=')[1].strip()
-                except: return last
-        return last
-                
-    def getGyro(self, last):
-        for data in self.datalist:
-            if 'gyro' in data:
-                try: return data.split('=')[1].strip()
-                except: return last
-        return last
 
     def getSonar(self, last):
         for data in self.datalist:
@@ -54,6 +41,17 @@ class Receiver():
                 try: return float(data.split('=')[1].strip())
                 except: return last
         return last
+
+
+    def getAccel(self, last):
+        for data in self.datalist:
+            if 'accel' in data:
+                try: return float(data.split('=')[1].strip())
+                except: return last
+        return last
+
+        #if len(self.datalist) > 0:
+            #return self.datalist[0].strip()
 
     def getAllData(self):
         if len(self.datalist) > 2:
