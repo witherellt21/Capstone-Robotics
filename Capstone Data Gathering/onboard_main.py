84f31fa0ff4fc094b659bbd84094df8cb76ba9dc
witherellt21 <witherellt21@mail.wlu.edu>
2020-11-12 12:52:57 -0500

    Update mainloop to support proximity sensor

--- Capstone Data Gathering/onboard_main.py
+++ Capstone Data Gathering/onboard_main.py
@@ -13,118 +13,105 @@ from IRsensor import IR
 from adafruit_motorkit import MotorKit
 
 
-# ---------------- Initialize Server -----------------
+sonar_activated = True
+imu_activated = True
+ir_sensor_activated = True
+motors_running = False
+server_online = False
 
-# Set the client to the server's IP and PORT address
-try:
-    IP = '192.168.0.3'
+
+# ---------------- Initialize Server -----------------
+if server_online:
+    # Set the client to the server's IP and PORT address
+    IP = '192.168.2.2'
     PORT = 10000
     server = Server(IP, PORT)
-except:
-    try:
-        IP = '192.168.0.3'
-        PORT = 10001
-        server = Server(IP, PORT)
-    except:
-        pass
 
+    server.start()
+    server.receiveConnection()
 
-server.start()
-server.receiveConnection()
-
-print('Connection Received')
-
-sonar_status = 'active'
-imu_status = 'active'
-ir_sensor_status = 'inactive'
-motor_status = 'inactive'
+    print('Connection Received')
 
 
 # ----------------- Initialize Sonar -----------------
-if sonar_status == 'active':
+if sonar_activated:
     s = Sonar(18, 24)
 
 
 # ------------------ Initialize IMU ------------------
-if imu_status == 'active':
+if imu_activated:
     imu = IMU()
 
 
 # ------------------ Initialize IR -------------------
-if ir_sensor_status == 'active':
+if ir_sensor_activated:
     ir = IR(17)
 
 
 # ---------------- Initialize Motors -----------------
-if motor_status == 'active':
+if motors_running:
     robot = MotorKit()
 
 dist = ''
 temp = ''
 gyro = ''
 acc = ''
-msg = ""
+msg = ''
 
 control = 'stop'
 
 running = True
 while running:
 
-    if sonar_status == 'active':
+    if sonar_activated:
         dist = round(s.distance(), 2)   # Get sonar distance data
 
-
         if dist <= 6:
-            if motor_status == 'active':
+            if motors_running:
                 control = 'stop'
 
-    if imu_status == 'active':
+    if imu_activated:
         ag_data_ready = imu.driver.read_ag_status().accelerometer_data_available
         if ag_data_ready:
             temp, acc, gyro = imu.read_ag()   # Get IMU data
 
-    if ir_sensor_status == 'active':
-        pass
-        #print(ir.status())   # Print status of proximity sensor
+    if ir_sensor_activated:
+        print(ir.status())   # Print status of proximity sensor
 
     # Compile a data string to send to the client
-    msg = "sonar = " + str(dist) + ",, temp = " + str(temp) + ",, accel = " + str(acc)+ ",, gyro = " + str(gyro)
-    #tag = str(len(msg))
-    #msg = tag + msg
-    print(str(msg))
+    msg = "sonar = " + str(dist) + ",, temp = " + str(temp) + ",, accel = " + str(acc) + \
+            ",, gyro = " + str(gyro) + ",, ir = " + str(ir.status())
+    print(msg)
 
-    # If client disconnects from server, reconnect
-    if server.disconnect_counter > 0:
-        server.receiveConnection()
+    if server_online:
+        # If client disconnects from server, reconnect
+        if server.disconnect_counter > 0:
+            server.receiveConnection()
+        server.send(msg)
+        #control = server.receive()
 
-    server.send(msg)
-    time.sleep(.3)
+        #print(control)
 
-    print('here')
-    control = server.receive()
+        if motors_running:
+            '''
+            # Wheels are turned at the same ratio as the joystick is held
+            robot.motor1.throttle = control # Right side wheels
+            robot.motor2.throttle = control
+            robot.motor3.throttle = -control # Left side wheels are turned opposite to the right side wheels
+            robot.motor4.throttle = -control
+            '''
 
-    print(control)
-
-    if motor_status == 'active':
-        '''
-        # Wheels are turned at the same ratio as the joystick is held
-        robot.motor1.throttle = control # Right side wheels
-        robot.motor2.throttle = control
-        robot.motor3.throttle = -control # Left side wheels are turned opposite to the right side wheels
-        robot.motor4.throttle = -control
-        '''
-
-        if control == 'forward':
-            robot.motor1.throttle = .6
-            robot.motor2.throttle = .6
-            robot.motor3.throttle = .6
-            robot.motor4.throttle = .6
-        elif control == 'stop':
-            robot.motor1.throttle = 0
-            robot.motor2.throttle = 0
-            robot.motor3.throttle = 0
-            robot.motor4.throttle = 0
-
+            if control == 'forward':
+                robot.motor1.throttle = .6
+                robot.motor2.throttle = .6
+                robot.motor3.throttle = .6
+                robot.motor4.throttle = .6
+            elif control == 'stop':
+                robot.motor1.throttle = 0
+                robot.motor2.throttle = 0
+                robot.motor3.throttle = 0
+                robot.motor4.throttle = 0
+    time.sleep(1)
     msg = ""
 
 print('done')
